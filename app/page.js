8433fb14a1f1f5bef65a91560f3951c31b8cddcf
jackual <162829405+jackual@users.noreply.github.com>
2025-09-29 12:51:59 +0100

    basic track

--- app/page.js
+++ app/page.js
@@ -1,5 +1,45 @@
+const trackRegions = [
+  {
+    name: 'bar',
+    length: 4,
+    position: 0,
+  },
+  {
+    name: 'half1',
+    length: 2,
+    position: 5
+  },
+  {
+    name: 'half2',
+    length: 2,
+    position: 10
+  }
+]
+
+let beatWidth = 40
+
+function renderTrackRegions() {
+  return trackRegions.map((region, index) => (
+    <div className="region" style={{
+      position: 'absolute',
+      width: beatWidth * region.length,
+      left: beatWidth * region.position,
+      top: 0
+    }} key={index}>
+      {region.name}
+    </div>
+  ));
+}
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <div className="trackContainer">
+      <div className="trackHeader">
+        <p>track1</p>
+      </div>
+      <div className="track" style={{ position: 'relative' }}>
+        {renderTrackRegions()}
+      </div>
+    </div>
   )
 }
