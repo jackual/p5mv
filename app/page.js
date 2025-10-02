474cad91f37f847059317893f81db8bc6bdd184e
jackual <162829405+jackual@users.noreply.github.com>
2025-10-02 18:11:17 +0100

    valito

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { proxy, useSnapshot } from 'valtio'
 
 const saveData = '{"tracks":[{"name":"top","regions":[{"name":"a","length":4,"position":0},{"name":"b","length":2,"position":5},{"name":"c","length":2,"position":10}]},{"name":"layer1","regions":[{"name":"intro","length":8,"position":0},{"name":"verse","length":16,"position":8},{"name":"chorus","length":8,"position":24},{"name":"bridge","length":4,"position":32}]},{"name":"text","regions":[{"name":"pattern1","length":12,"position":0},{"name":"pattern2","length":8,"position":12},{"name":"breakdown","length":4,"position":20}]},{"name":"background","regions":[{"name":"verse1","length":16,"position":8},{"name":"chorus1","length":8,"position":24},{"name":"verse2","length":16,"position":40},{"name":"outro","length":6,"position":56}]}]}'
 
@@ -65,19 +65,20 @@ class Region {
   }
 }
 
-export default function Home() {
-  const [project, setProject] = useState(() => new Project(saveData))
+const project = proxy(new Project(saveData))
 
-  function renderTrackRegions(trackRegions) {
+export default function Home() {
+  const snap = useSnapshot(project)
+
+  function renderTrackRegions(trackRegions, trackIndex) {
     return trackRegions.map((region, index) => (
       <div className={"region" + (region.selected ? " selected" : "")} onClick={event => {
-        region.select(event)
-        setProject({ ...project }
-        )
+        // Access the original proxy object for mutations, not the snapshot
+        project.tracks[trackIndex].regions[index].select(event)
       }} style={{
         position: 'absolute',
-        width: project.view.beatWidth * region.length,
-        left: project.view.beatWidth * region.position,
+        width: snap.view.beatWidth * region.length,
+        left: snap.view.beatWidth * region.position,
         top: 0
       }} key={index}>
         {region.name}
@@ -92,7 +93,7 @@ export default function Home() {
           <p>{track.name}</p>
         </div>
         <div className="track" style={{ position: 'relative' }}>
-          {renderTrackRegions(track.regions)}
+          {renderTrackRegions(track.regions, index)}
         </div>
       </div>
     ))
@@ -100,7 +101,7 @@ export default function Home() {
 
   return (
     <div>
-      {renderTracks(project.tracks)}
+      {renderTracks(snap.tracks)}
     </div>
   )
 }
