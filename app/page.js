46d9371964a74fae8f0fb2f4b0bd92f0fb67d4be
jackual <162829405+jackual@users.noreply.github.com>
2025-10-02 17:34:48 +0100

    usestate

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from 'react'
+
 const saveData = '{"tracks":[{"name":"top","regions":[{"name":"a","length":4,"position":0},{"name":"b","length":2,"position":5},{"name":"c","length":2,"position":10}]},{"name":"layer1","regions":[{"name":"intro","length":8,"position":0},{"name":"verse","length":16,"position":8},{"name":"chorus","length":8,"position":24},{"name":"bridge","length":4,"position":32}]},{"name":"text","regions":[{"name":"pattern1","length":12,"position":0},{"name":"pattern2","length":8,"position":12},{"name":"breakdown","length":4,"position":20}]},{"name":"background","regions":[{"name":"verse1","length":16,"position":8},{"name":"chorus1","length":8,"position":24},{"name":"verse2","length":16,"position":40},{"name":"outro","length":6,"position":56}]}]}'
 
 class Track {
@@ -45,37 +47,38 @@ class Region {
   }
 }
 
-const project = new Project(saveData)
-
-function renderTrackRegions(trackRegions) {
-  return trackRegions.map((region, index) => (
-    <div className={"region" + (region.selected ? " selected" : "")} onClick={() => {
-      region.select()
-    }} style={{
-      position: 'absolute',
-      width: project.view.beatWidth * region.length,
-      left: project.view.beatWidth * region.position,
-      top: 0
-    }} key={index}>
-      {region.name}
-    </div>
-  ));
-}
-
-function renderTracks(tracks) {
-  return tracks.map((track, index) => (
-    <div className="trackContainer" key={index}>
-      <div className="trackHeader">
-        <p>{track.name}</p>
-      </div>
-      <div className="track" style={{ position: 'relative' }}>
-        {renderTrackRegions(track.regions)}
-      </div>
-    </div>
-  ))
-}
-
 export default function Home() {
+  const [project, setProject] = useState(() => new Project(saveData))
+
+  function renderTrackRegions(trackRegions) {
+    return trackRegions.map((region, index) => (
+      <div className={"region" + (region.selected ? " selected" : "")} onClick={() => {
+        region.select()
+        setProject({ ...project }) // Trigger re-render
+      }} style={{
+        position: 'absolute',
+        width: project.view.beatWidth * region.length,
+        left: project.view.beatWidth * region.position,
+        top: 0
+      }} key={index}>
+        {region.name}
+      </div>
+    ));
+  }
+
+  function renderTracks(tracks) {
+    return tracks.map((track, index) => (
+      <div className="trackContainer" key={index}>
+        <div className="trackHeader">
+          <p>{track.name}</p>
+        </div>
+        <div className="track" style={{ position: 'relative' }}>
+          {renderTrackRegions(track.regions)}
+        </div>
+      </div>
+    ))
+  }
+
   return (
     <div>
       {renderTracks(project.tracks)}
