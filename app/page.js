f8810b4a5afae470b44ecc2b964efcaf570bbef8
jackual <162829405+jackual@users.noreply.github.com>
2025-10-02 17:52:43 +0100

    select

--- app/page.js
+++ app/page.js
@@ -5,9 +5,10 @@ import { useState } from 'react'
 const saveData = '{"tracks":[{"name":"top","regions":[{"name":"a","length":4,"position":0},{"name":"b","length":2,"position":5},{"name":"c","length":2,"position":10}]},{"name":"layer1","regions":[{"name":"intro","length":8,"position":0},{"name":"verse","length":16,"position":8},{"name":"chorus","length":8,"position":24},{"name":"bridge","length":4,"position":32}]},{"name":"text","regions":[{"name":"pattern1","length":12,"position":0},{"name":"pattern2","length":8,"position":12},{"name":"breakdown","length":4,"position":20}]},{"name":"background","regions":[{"name":"verse1","length":16,"position":8},{"name":"chorus1","length":8,"position":24},{"name":"verse2","length":16,"position":40},{"name":"outro","length":6,"position":56}]}]}'
 
 class Track {
-  constructor(track) {
+  constructor(track, project) {
     this.name = track.name
-    this.regions = track.regions.map(region => new Region(region))
+    this.project = project
+    this.regions = track.regions.map(region => new Region(region, project))
   }
   export() {
     return {
@@ -20,22 +21,39 @@ class Track {
 class Project {
   constructor(string) {
     let data = JSON.parse(string)
-    this.tracks = data.tracks.map(track => new Track(track))
+    this.tracks = data.tracks.map(track => new Track(track, this))
     this.view = {
       beatWidth: 10
     }
   }
+  get selected() {
+    let selectedRegions = []
+    this.tracks.forEach(track => {
+      track.regions.forEach(region => {
+        if (region.selected) {
+          selectedRegions.push(region)
+        }
+      })
+    })
+    return selectedRegions
+  }
+  deselectAll() {
+    this.selected.forEach(region => region.selected = false)
+  }
 }
 
 class Region {
-  constructor(object) {
+  constructor(object, project) {
     this.name = object.name
     this.length = object.length
     this.position = object.position
     this.selected = object.selected || false
-    this.id = Math.random().toString(36).substring(2, 9) //could this be a symbol
+    this.id = Math.random().toString(36).substring(2, 9)
+    this.project = project
   }
-  select() {
+  select(event) {
+    if (!event?.shiftKey)
+      this.project.deselectAll()
     this.selected = !this.selected
   }
   export() {
@@ -52,9 +70,10 @@ export default function Home() {
 
   function renderTrackRegions(trackRegions) {
     return trackRegions.map((region, index) => (
-      <div className={"region" + (region.selected ? " selected" : "")} onClick={() => {
-        region.select()
-        setProject({ ...project }) // Trigger re-render
+      <div className={"region" + (region.selected ? " selected" : "")} onClick={event => {
+        region.select(event)
+        setProject({ ...project }
+        )
       }} style={{
         position: 'absolute',
         width: project.view.beatWidth * region.length,
