2ac59518c05a6a3693fff22ff152e787ef733a29
jackual <162829405+jackual@users.noreply.github.com>
2025-10-06 17:27:50 +0100

    ffmpeg script frame->video

--- public/scenes/blinds.js
+++ public/scenes/blinds.js
@@ -17,7 +17,7 @@ function draw() {
   }
   if (window.frameNumber < 10) {
     console.log("Capturing frame:", window.frameNumber)
-    saveCanvas('f' + window.frameNumber, 'png')
+    saveCanvas('f' + window.frameNumber.toString().padStart(4, '0'), 'png')
     console.log("Captured:", window.frameNumber)
     window.frameNumber++
   } else {
