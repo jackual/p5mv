c1ed6e4ff4cdff2dfcc361acdef5fedb57c5ac33
jackual <162829405+jackual@users.noreply.github.com>
2025-10-08 21:17:34 +0100

    inspector form placeholders

--- components/Inspector.js
+++ components/Inspector.js
@@ -1,8 +1,16 @@
 export default function Inspector({ project }) {
+    const form = {
+        region: () => (<h1>{project.selected[0].name}</h1>),
+        'same-track': () => (<h1>{project.selected.length} regions selected</h1>),
+        'whole-track': () => (<h1>Whole track selected</h1>),
+        mixed: () => (<h1>Mixed selection</h1>),
+        none: () => (<>
+            <h1>Project</h1>
+        </>)
+    }
     return (
         <div className="inspector">
-            <h2>{project.selectionMode}</h2>
-            <p>Details about the selected region will appear here.</p>
+            {form[project.selectionMode]()}
         </div>
     )
-} 
+}  
