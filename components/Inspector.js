e6e82cc583fb7989e154b34a62219805ec2ba612
jackual <162829405+jackual@users.noreply.github.com>
2025-11-03 13:55:35 +0000

    ui updates

--- components/Inspector.js
+++ components/Inspector.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import blendModes from '@/data/blendModes';
 import Blend from '@/lib/classes/Blend';
 import { beatsToMusicalTimeString, beatsToTimecode, timeReport } from '@/lib/timeUtils';
+import { GearIcon } from '@phosphor-icons/react';
 
 export default function Inspector({ project, snapshot }) {
     const {
@@ -190,7 +191,7 @@ export default function Inspector({ project, snapshot }) {
 
     const ProjectSettingsForm = () => (
         <div>
-            <h3>Project Settings</h3>
+            <h3 style={{ display: "flex", alignItems: "center" }}><GearIcon /> Project Settings</h3>
             <div>
                 <div className="form-group">
                     <label htmlFor="title">Title</label>
