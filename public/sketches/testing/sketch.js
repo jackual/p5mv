566552e83e0010e8f55d71add923d42cdb92ece7
jackual <162829405+jackual@users.noreply.github.com>
2025-11-13 17:42:07 +0000

    fix frame bug and added hex colours

--- public/sketches/testing/sketch.js
+++ public/sketches/testing/sketch.js
@@ -5,12 +5,10 @@ function setup() {
 }
 
 function draw() {
-    background(220);
+    background(p5mv.colour_test);
     try {
         // Debug what's in inputs
-        text(`Inputs: ${p5mv.number_test}`, 20, 20, 300, 100)
-        text(`Frame: ${frameCount}`, 20, 140, 100, 30)
-        text(`Test: ${Object.keys(p5mv).join(",")}`, 20, 180, 100, 30)
+        text(`Inputs: ${p5mv.colour_test} ${__frameIndex}`, 20, 20, 300, 100)
     } catch (e) {
         text(`Error: ${e.message}`, 20, 20, 200, 30)
     }
