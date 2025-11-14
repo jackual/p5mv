649a1fb122a77af25115711121d2d2c9fc70a37b
jackual <162829405+jackual@users.noreply.github.com>
2025-11-14 18:29:27 +0000

    p5mv setup and props

--- public/sketches/testing/sketch.js
+++ public/sketches/testing/sketch.js
@@ -1,22 +1,13 @@
 function setup() {
-    createCanvas(...captureInput.dims)
-    pixelDensity(1)
-    noLoop()
+    p5mv.setup()
 }
 
 function draw() {
     background(p5mv.colour_test);
     try {
         // Debug what's in inputs
-        // text(`Inputs: ${p5mv.number_test} ${__frameIndex}`, 20, 20, 300, 100)
-
-        circle(p5mv.number_test, height / 4, 50);
-        circle(p5mv.float_test, height / 5, 50);
+        text(`Inputs: ${p5mv.colour_test} ${Math.floor(p5mv.percentElapsed)}%`, 20, 20, 300, 100)
     } catch (e) {
         text(`Error: ${e.message}`, 20, 20, 200, 30)
     }
 }
-
-window.renderFrame = (i) => {
-    redraw();
-};
