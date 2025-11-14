1509cdc0d43cef834286a6b06570d4c454102a55
jackual <162829405+jackual@users.noreply.github.com>
2025-11-14 18:17:29 +0000

    eases in renderer

--- public/sketches/testing/sketch.js
+++ public/sketches/testing/sketch.js
@@ -8,7 +8,10 @@ function draw() {
     background(p5mv.colour_test);
     try {
         // Debug what's in inputs
-        text(`Inputs: ${p5mv.colour_test} ${__frameIndex}`, 20, 20, 300, 100)
+        // text(`Inputs: ${p5mv.number_test} ${__frameIndex}`, 20, 20, 300, 100)
+
+        circle(p5mv.number_test, height / 4, 50);
+        circle(p5mv.float_test, height / 5, 50);
     } catch (e) {
         text(`Error: ${e.message}`, 20, 20, 200, 30)
     }
