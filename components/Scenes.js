abb2871bd7a527042e7059c3c2723cf100c6f0ba
jackual <162829405+jackual@users.noreply.github.com>
2025-11-16 23:27:02 +0000

    scene deselect logic

--- components/Scenes.js
+++ components/Scenes.js
@@ -5,6 +5,17 @@ import { useState } from 'react'
 
 export default function Scenes() {
     const [selectedSketch, setSelectedSketch] = useState(null)
+    const handleClick = (e) => {
+        const li = e.target.closest('li')
+        if (li) {
+            const sketchId = li.getAttribute('data-sketchid')
+            if (sketchId !== selectedSketch) {
+                setSelectedSketch(sketchId)
+                return
+            }
+        }
+        setSelectedSketch(null)
+    }
 
     return (
         <div className='scenePage'>
@@ -12,11 +23,11 @@ export default function Scenes() {
                 <IconText as="h1" icon={SquaresFourIcon}>
                     Scenes Library
                 </IconText>
-                <ul className='sketch-list'>
+                <ul className='sketch-list' onClick={handleClick}>
                     {sketches.map(sketch => (
                         <li
                             key={sketch.id}
-                            onClick={() => setSelectedSketch(sketch.id)}
+                            data-sketchid={sketch.id}
                             className={selectedSketch === sketch.id ? 'selected' : ''}
                         >
                             <img src={`/sketches/${sketch.id}/${sketch.thumb}`} alt={sketch.title} />
