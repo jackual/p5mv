a9950dde3ca2b6656583e4b32eb7ae25b07032f9
jackual <162829405+jackual@users.noreply.github.com>
2025-11-16 23:22:09 +0000

    added ribbon

---
+++ public/sketches/ribbon/info.js
@@ -0,0 +1,12 @@
+export default {
+    title: "Ribbon",
+    thumb: "ribbon.png",
+    inputs: [
+        {
+            id: "strokeWeight",
+            label: "Stroke Weight",
+            type: "float",
+            default: 1.4
+        }
+    ]
+}
