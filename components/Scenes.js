b1945d2c157932fb28b326838ec443a883d19b59
jackual <162829405+jackual@users.noreply.github.com>
2025-11-16 23:22:34 +0000

    select a sketch in library and styling

--- components/Scenes.js
+++ components/Scenes.js
@@ -1,29 +1,37 @@
 import sketches from '../data/sketches'
 import IconText from './IconText'
 import { SquaresFourIcon } from '@phosphor-icons/react'
+import { useState } from 'react'
 
 export default function Scenes() {
+    const [selectedSketch, setSelectedSketch] = useState(null)
+
     return (
         <div className='scenePage'>
-            <div>
+            <div id='scene-main'>
                 <IconText as="h1" icon={SquaresFourIcon}>
                     Scenes Library
                 </IconText>
                 <ul className='sketch-list'>
                     {sketches.map(sketch => (
-                        <li key={sketch.id}>
+                        <li
+                            key={sketch.id}
+                            onClick={() => setSelectedSketch(sketch.id)}
+                            className={selectedSketch === sketch.id ? 'selected' : ''}
+                        >
                             <img src={`/sketches/${sketch.id}/${sketch.thumb}`} alt={sketch.title} />
                             <div className='caption'>
                                 <div className='blob' style={{ backgroundColor: sketch.color }}></div>
                                 <p>{sketch.title}</p>
                             </div>
-
                         </li>
                     ))}
                 </ul>
             </div>
             <aside>
-                <p>Select a scene</p>
+                {selectedSketch
+                    ? <p>Selected scene: {sketches.find(s => s.id === selectedSketch)?.title}</p>
+                    : <p>Select a scene</p>}
             </aside>
         </div>
     )
