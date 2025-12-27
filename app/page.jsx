760288004252ba5b265eaa320c510652d781f1ea
jackual <162829405+jackual@users.noreply.github.com>
2025-12-27 16:06:08 +0000

    seperate file methods

--- app/page.jsx
+++ app/page.jsx
@@ -10,63 +10,15 @@ import Render from '@/components/Render'
 import Help from '@/components/Help'
 import Scenes from '@/components/Scenes'
 import Ribbon from '@/components/Ribbon'
+import { newFile, openFile, saveFile } from '@/lib/projectFileMethods'
 
 const project = proxy(new Project())
 
+// Bind project to file methods
 const projectFileMethods = {
-  newFile: () => {
-    const newProject = new Project('{}')
-
-    // Clear all existing properties
-    Object.keys(project).forEach(key => {
-      delete project[key]
-    })
-
-    // Add all new project properties
-    Object.assign(project, newProject)
-  },
-  openFile: () => {
-    const input = document.createElement('input')
-    input.type = 'file'
-    input.accept = '.p5mvProject'
-    input.onchange = (e) => {
-      const file = e.target.files[0]
-      if (!file) return
-      
-      const reader = new FileReader()
-      reader.onload = (event) => {
-        try {
-          const loadedProject = new Project(event.target.result)
-          
-          // Clear all existing properties
-          Object.keys(project).forEach(key => {
-            delete project[key]
-          })
-          
-          // Add all loaded project properties
-          Object.assign(project, loadedProject)
-        } catch (error) {
-          alert('Error loading project: ' + error.message)
-        }
-      }
-      reader.readAsText(file)
-    }
-    input.click()
-  },
-  saveFile: () => {
-    const saveData = JSON.stringify(project.export())
-    function downloadObjectAsJson(exportObj, exportName) {
-      // https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
-      var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
-      var downloadAnchorNode = document.createElement('a');
-      downloadAnchorNode.setAttribute("href", dataStr);
-      downloadAnchorNode.setAttribute("download", exportName);
-      document.body.appendChild(downloadAnchorNode); // required for firefox
-      downloadAnchorNode.click();
-      downloadAnchorNode.remove();
-    }
-    downloadObjectAsJson(project.export(), project.meta.title + ".p5mvProject")
-  }
+  newFile: () => newFile(project),
+  openFile: () => openFile(project),
+  saveFile: () => saveFile(project)
 }
 
 // Auto-save project data whenever it changes
