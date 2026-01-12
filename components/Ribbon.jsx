5afd2c70a5f5bf8acc6d65499263ce39b9f04f03
jackual <162829405+jackual@users.noreply.github.com>
2026-01-12 02:01:10 +0000

    ribbon update

--- components/Ribbon.jsx
+++ components/Ribbon.jsx
@@ -24,7 +24,7 @@ import Project from "@/lib/classes/Project"
 const RibbonButton = ({ icon: Icon, label, onClick = () => { } }) => {
     return (
         <div className="ribbon-button" onClick={onClick} title={label}>
-            <Icon size={24} weight="light" />
+            <Icon size={20} weight="light" />
         </div>
     )
 }
@@ -32,7 +32,8 @@ const RibbonButton = ({ icon: Icon, label, onClick = () => { } }) => {
 const RibbonRadio = ({ icon: Icon, page, label, isSelected, onClick }) => {
     return (
         <div className={`ribbon-radio ${isSelected ? 'ribbon-selected' : ''}`} onClick={onClick} id={page + "-radio"} title={label || capitalize(page)}>
-            <Icon size={24} weight={isSelected ? "regular" : "light"} />
+            <Icon size={18} weight="light" />
+            <span className="ribbon-radio-label">{label || capitalize(page)}</span>
         </div>
     )
 }
@@ -48,45 +49,54 @@ const Ribbon = ({ page, setPage, project, projectFileMethods }) => {
         <div className="ribbon">
             <img src="./logo.svg" className="ribbon-logo" />
             <Divider />
-            <RibbonButton icon={FilePlusIcon} label="New" onClick={() => {
-                confirm("Are you sure you want to create a new project?") && projectFileMethods.newFile()
-            }} />
-            <RibbonButton icon={FolderOpenIcon} label="Open" onClick={() => projectFileMethods.openFile()} />
-            <RibbonButton icon={FloppyDiskIcon} label="Save" onClick={() => projectFileMethods.saveFile()} />
+            <RibbonRadio icon={FilmStripIcon} page="timeline" label="Timeline" isSelected={page === "timeline"} onClick={() => setPage("timeline")} />
+            <RibbonRadio icon={SquaresFourIcon} page="scenes" label="Scenes" isSelected={page === "scenes"} onClick={() => setPage("scenes")} />
+            <RibbonRadio icon={CodeIcon} page="editor" label="Editor" isSelected={page === "editor"} onClick={() => setPage("editor")} />
+            <RibbonRadio icon={ExportIcon} page="render" label="Render" isSelected={page === "render"} onClick={() => setPage("render")} />
+            <RibbonRadio icon={QuestionIcon} page="help" label="Help" isSelected={page === "help"} onClick={() => setPage("help")} />
             <Divider />
-            <RibbonButton icon={MagnifyingGlassPlusIcon} label="Zoom In" onClick={() => project.zoomIn()} />
-            <RibbonButton icon={MagnifyingGlassMinusIcon} label="Zoom Out" onClick={() => project.zoomOut()} />
-            <RibbonButton icon={ArrowLineLeftIcon} label="Move to start" onClick={() => project.moveToStart()} />
-            <RibbonButton icon={ArrowLeftIcon} label="Left" onClick={() => project.moveLeft()} />
-            <RibbonButton icon={ArrowRightIcon} label="Right" onClick={() => project.moveRight()} />
-            <Divider />
-            <div className="ribbon-snap-control">
-                <label htmlFor="ribbon-snap">Snap:</label>
-                <select
-                    id="ribbon-snap"
-                    value={project.snap}
-                    onChange={(e) => {
-                        project.snap = parseFloat(e.target.value);
-                    }}
-                >
-                    {[4, 1, 0.5, 1 / 3, 0.25, 0.125].map(value => (
-                        <option key={value} value={value}>
-                            {beatsToMusicalTimeString(value)}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <Divider />
-            <RibbonButton icon={CopyIcon} label="Copy" onClick={() => project.copy()} />
-            <RibbonButton icon={ClipboardTextIcon} label="Paste" onClick={() => project.paste()} />
-            <RibbonButton icon={SelectionAllIcon} label="Select All" onClick={() => project.selectAll()} />
-            <RibbonButton icon={TrashIcon} label="Delete" />
-            <Divider />
-            <RibbonRadio icon={FilmStripIcon} page="timeline" isSelected={page === "timeline"} onClick={() => setPage("timeline")} />
-            <RibbonRadio icon={SquaresFourIcon} page="scenes" isSelected={page === "scenes"} onClick={() => setPage("scenes")} />
-            <RibbonRadio icon={CodeIcon} page="editor" isSelected={page === "editor"} onClick={() => setPage("editor")} />
-            <RibbonRadio icon={ExportIcon} page="render" isSelected={page === "render"} onClick={() => setPage("render")} />
-            <RibbonRadio icon={QuestionIcon} page="help" label="Documentation" isSelected={page === "help"} onClick={() => setPage("help")} />
+            {page === "timeline" && (
+                <>
+                    <RibbonButton icon={FilePlusIcon} label="New" onClick={() => {
+                        confirm("Are you sure you want to create a new project?") && projectFileMethods.newFile()
+                    }} />
+                    <RibbonButton icon={FolderOpenIcon} label="Open" onClick={() => projectFileMethods.openFile()} />
+                    <RibbonButton icon={FloppyDiskIcon} label="Save" onClick={() => projectFileMethods.saveFile()} />
+                    <Divider />
+                    <RibbonButton icon={MagnifyingGlassPlusIcon} label="Zoom In" onClick={() => project.zoomIn()} />
+                    <RibbonButton icon={MagnifyingGlassMinusIcon} label="Zoom Out" onClick={() => project.zoomOut()} />
+                    <RibbonButton icon={ArrowLineLeftIcon} label="Move to start" onClick={() => project.moveToStart()} />
+                    <RibbonButton icon={ArrowLeftIcon} label="Left" onClick={() => project.moveLeft()} />
+                    <RibbonButton icon={ArrowRightIcon} label="Right" onClick={() => project.moveRight()} />
+                    <Divider />
+                    <div className="ribbon-snap-control">
+                        <label htmlFor="ribbon-snap">Snap:</label>
+                        <select
+                            id="ribbon-snap"
+                            value={project.snap}
+                            onChange={(e) => {
+                                project.snap = parseFloat(e.target.value);
+                            }}
+                        >
+                            {[4, 1, 0.5, 1 / 3, 0.25, 0.125].map(value => (
+                                <option key={value} value={value}>
+                                    {beatsToMusicalTimeString(value)}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <Divider />
+                    <RibbonButton icon={CopyIcon} label="Copy" onClick={() => project.copy()} />
+                    <RibbonButton icon={ClipboardTextIcon} label="Paste" onClick={() => project.paste()} />
+                    <RibbonButton icon={SelectionAllIcon} label="Select All" onClick={() => project.selectAll()} />
+                    <RibbonButton icon={TrashIcon} label="Delete" />
+                </>
+            )}
+            {page === "editor" && (
+                <div className="ribbon-message">
+                    Download sketch to add to your project
+                </div>
+            )}
         </div>
     )
 }
